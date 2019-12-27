c8391eaa166b3109272dc948bad64169520d3235
Johann Feser <johannfeser@gmail.com>
2019-12-28 00:37:02 +0100

    Update navigation.dart

--- lib/main/navigation.dart
+++ lib/main/navigation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_util/flutter_web_util.dart';
+import 'package:personal_website/other/language_provider.dart';
 
 class SiteNavigation extends StatelessWidget {
   final MainAxisAlignment mainAxisAlignment;
@@ -19,45 +20,80 @@ class SiteNavigation extends StatelessWidget {
       mainAxisAlignment: mainAxisAlignment,
       crossAxisAlignment: crossAxisAlignment,
       children: <Widget>[
-        Padding(
+        NavButton(
+          text: 'Home',
+          onPressed: () {
+            Navigator.pushNamed(context, '/');
+          },
           padding: EdgeInsets.only(
             right: padding,
           ),
-          child: MousePointer(
-            child: FlatButton(
-              child: Text('Home'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/');
-              },
-            ),
-          ),
         ),
-        Padding(
+        NavButton(
+          text: 'Projects',
+          onPressed: () {
+            Navigator.pushNamed(context, '/projects');
+          },
           padding: EdgeInsets.only(
             left: padding,
             right: padding,
           ),
-          child: MousePointer(
-            child: FlatButton(
-              child: Text('Projects'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/projects');
-              },
-            ),
+        ),
+        NavButton(
+          text: 'Contact',
+          onPressed: () {
+            Navigator.pushNamed(context, '/contact');
+          },
+          padding: EdgeInsets.only(
+            left: padding,
+            right: padding,
           ),
         ),
         Padding(
           padding: EdgeInsets.only(left: padding),
-          child: MousePointer(
-            child: FlatButton(
-              child: Text('Contact'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/contact');
-              },
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: 8,
+              vertical: 16,
             ),
+            child: LanguageSwitch(),
           ),
         ),
       ],
+    );
+  }
+}
+
+class NavButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final EdgeInsetsGeometry padding;
+
+  const NavButton({
+    Key key,
+    @required this.text,
+    @required this.onPressed,
+    this.padding = const EdgeInsets.all(0.0),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: padding,
+      child: HoverColorWidget(
+        builder: (BuildContext context, Color value) {
+          return FlatButton(
+            hoverColor: Colors.transparent,
+            textColor: value,
+            child: Text(
+              text,
+            ),
+            onPressed: onPressed,
+          );
+        },
+        value: Colors.white,
+        hoverValue: Colors.green[300],
+      ),
     );
   }
 }
