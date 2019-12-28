dff42eba508483ebbfb7c88b672ddf4d64d72c34
Johann Feser <johannfeser@gmail.com>
2019-12-28 21:54:05 +0100

    Update contact_page.dart

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_web_util/flutter_web_util.dart';
 import 'package:personal_website/app_localizations.dart';
 import 'package:personal_website/other/markdown_widget.dart';
+import 'package:screen_size_util/screen_size_util.dart';
 
 class ContactPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ResponsiveWidget(
-      desktop: (_) => _Content(),
-      mobile: (_) => _Content(
+    return ResponsiveBuilder(
+      large: (_) => _Content(),
+      extraSmall: (_) => _Content(
         fontSize: 16,
       ),
     );
@@ -27,13 +27,15 @@ class _Content extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
         Text(
-          AppLocalizations.of(context).translations['contact'],
-          style: TextStyle(fontSize: fontSize),
+          AppLocalizations.of(context).translations['menu']['contact'],
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        LinkWidget(
-          text: 'Email',
-          url: 'mailto:johannfeser@gmail.com',
-          linkStyle: TextStyle(fontSize: fontSize),
+        Markdown(
+          AppLocalizations.of(context).translations['contact'],
+          textStyle: TextStyle(fontSize: fontSize),
         ),
       ],
     );
