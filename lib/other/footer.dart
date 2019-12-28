e32c9051bc661ab9eb664d0efb8e3e049cfea455
Johann Feser <johannfeser@gmail.com>
2019-12-28 21:54:00 +0100

    Update footer.dart

--- lib/other/footer.dart
+++ lib/other/footer.dart
@@ -1,30 +1,23 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:personal_website/other/f_animation.dart';
 import 'package:personal_website/other/markdown_widget.dart';
 import 'package:screen_size_util/screen_size_util.dart';
-
 
 class Footer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: BoxConstraints(
-          minHeight: MediaQuery.of(context).height * 0.1,
-          maxHeight: MediaQuery.of(context).height * 0.2),
-      child: Card(
-        child: Center(
-          child: LayoutBuilder(builder: (context, constraints) {
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: constraints.maxHeight,
-                  height: constraints.maxHeight,
-                  child: FAnimation(),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return ResponsiveBuilder(
+      extraSmall: (_) {
+        return Container(
+          height:
+              max(MediaQuery.of(context).height, MediaQuery.of(context).width) *
+                  0.1,
+          child: Card(
+            child: Center(
+              child: LayoutBuilder(builder: (context, constraints) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     Text('Made by Johann Feser'),
@@ -34,16 +27,35 @@ class Footer extends StatelessWidget {
                       linkStyle: TextStyle(),
                     ),
                   ],
-                ),
-                Container(
-                  width: constraints.maxHeight,
-                  height: constraints.maxHeight,
-                ),
-              ],
-            );
-          }),
-        ),
-      ),
+                );
+              }),
+            ),
+          ),
+        );
+      },
+      large: (_) {
+        return Container(
+          height: MediaQuery.of(context).height * 0.1,
+          child: Card(
+            child: Center(
+              child: LayoutBuilder(builder: (context, constraints) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Made by Johann Feser'),
+                    LinkWidget(
+                      text: 'With Flutter',
+                      url: 'https://flutter.dev',
+                      linkStyle: TextStyle(),
+                    ),
+                  ],
+                );
+              }),
+            ),
+          ),
+        );
+      },
     );
   }
 }
